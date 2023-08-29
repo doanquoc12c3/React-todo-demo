4ff9581d8ac6fedc4a90393ea41f030416ca31dc
unknown <doanquoc12c3>
2023-08-30 04:25:58 +0800

    add delete+add function

--- src/App.js
+++ src/App.js
@@ -3,23 +3,30 @@ import './App.css';
 import TodoTable from './components/TodoTable';
 
 function App() {
-  const todos = [
+  const[todos, setTodos] = useState( [
     {rowNumber:1, rowDescription: 'Feed puppy', rowAssigned: 'User one'},
     {rowNumber:2, rowDescription: 'Make plants', rowAssigned: 'User two'},
     {rowNumber:3, rowDescription: 'Make dinners', rowAssigned: 'User three'}
-
-  ]
+  ])
+  let a = 0;
 
   const addTodo = () => {
-    if(todos.length > 0){
+    
       const newTodo = {
         rowNumber: todos.length + 1,
         rowDescription: 'New Todo',
         rowAssigned: 'User Three'
       };
-      todos.push(newTodo);
+      setTodos(todos => [...todos, newTodo]);
       console.log(todos);
-    }
+    
+  };
+
+  const deleteTodo = () => {
+
+      setTodos(todos => todos.filter((ele)=>{
+        return ele !== todos[todos.length-1]
+      }))
   }
 
   return (
@@ -28,11 +35,14 @@ function App() {
       <div className="card-header">
         Your Todo's
       </div>
-      <div>
+      <div class="row justify-content-between">
             <TodoTable todos={todos}/>
-            <button className='btn btn-primary' onClick={addTodo}>
+            <button className='col-4 btn btn-primary' class="col-4" onClick={addTodo}>
               Add new todo
               </button>
+            <button className='col-4 btn btn-danger' class="col-4" onClick={deleteTodo}>
+              Delete last todo
+            </button>
       </div>
       </div>
       
